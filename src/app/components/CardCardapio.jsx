5b6de17f252d2ac0d6667d3d29b261df309574ca
ezno02 <enzo.bmachado.e@gmail.com>
2025-11-20 15:32:43 -0300

    estilizacao inicial do cardapio(apenas mobile)

--- src/app/components/CardCardapio.jsx
+++ src/app/components/CardCardapio.jsx
@@ -3,11 +3,10 @@ import Image from 'next/image'
 import styles from './css/CardCardapio.module.css'
 
 const CardCardapio = ({ nome, desc, img, preco, id }) => {
-    console.log(nome)
     return (
         <div className={styles.card}>
             <Image src={img} alt={`Imagem do ${nome}`} />
-            <div className="text">
+            <div className={styles.text}>
                 <h3>{nome}</h3>
                 <p>{desc}</p>
                 <p className={styles.preco}>{preco}</p>
